8ab406457378554f4dc181d6464643d72baa4ae3
merveille <merveillechabi2002@gmail.com>
2023-10-18 17:45:21 +0100

    mise a jour

---
+++ resources/views/employe/customer-list.blade.php
@@ -0,0 +1,5 @@
+@extends("layouts.app")
+
+@section("content")
+    <livewire:employe.customer-list-component />
+@endsection
---
+++ resources/views/employe/loan-request.blade.php
@@ -0,0 +1,5 @@
+@extends("layouts.app")
+
+@section("content")
+    <livewire:employe.loan-request-component />
+@endsection

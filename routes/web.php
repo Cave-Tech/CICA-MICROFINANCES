d4ad0c8486ef6296a78ea20138249711ecbb53eb
lionel <lokononlionel97@gmail.com>
2023-10-18 13:07:33 +0200

    modification

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,44 @@ Route::get('/', function () {
     return view('livewire.dashboard-component');
 })->name('dashboard');
 
+<<<<<<< Updated upstream
+=======
+Route::get('/client-dashboard', function () {
+    return view('client.dashboard');
+})->name('client.dashboard');
+
+Route::get('/client-profile', function () {
+    return view('client.profile');
+})->name('client.profile');
+
+Route::get('/client-loan-request', function () {
+    return view('client.loan-request');
+})->name('client.loan-request');
+
+
+
+
+
+Route::get('/employe-dashboard', function () {
+    return view('employe.dashboard');
+})->name('employe.dashboard');
+
+
+Route::get('/employe-profile', function () {
+    return view('employe.profile');
+})->name('employe.profile');
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+require __DIR__.'/auth.php';
+>>>>>>> Stashed changes

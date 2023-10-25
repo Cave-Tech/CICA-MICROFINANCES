5746ebc4dc3145dbd13c0afeab658bf8d36b7f1d
Fadeilou ALASSANE <afadeilou@gmail.com>
2023-10-25 12:16:47 +0100

    Ajout de seeder pour la base de donnee

---
+++ database/seeders/AccountSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Account;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AccountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $clientUsers = User::where('profile_id', 3)->get();
+
+        foreach ($clientUsers as $client) {
+            Account::create([
+                'users_id' => $client->id,
+                'account_types_id' => rand(1, 2), // Pour varier entre compte courant et épargne
+                'balance' => rand(1000, 10000),  // Solde entre 1000 et 10000
+                'interest_rate' => rand(1, 5),   // Taux d'intérêt entre 1% et 5%
+                'opening_date' => now(),
+                'status' => 'active'
+            ]);
+        }
+    }
+}
---
+++ database/seeders/LoanSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Loan;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LoanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $clientUsers = User::where('profile_id', 3)->get();
+        $statuses = ['pending', 'validated', 'rejected'];
+
+        foreach ($clientUsers as $client) {
+            foreach (range(1, 20) as $index) {
+                $loanDate = now();  // Date actuelle
+                $paymentFrequency = rand(1, 12);  // Fréquence de paiement entre 1 et 12 mois
+                $dueDate = $loanDate->copy()->addMonths($paymentFrequency);  // Date d'échéance après les mois de fréquence de paiement
+
+                Loan::create([
+                    'borrower_id' => $client->id,
+                    'agent_id' => User::where('employee_type_id', 2)
+                                    ->orWhere('employee_type_id', 4)
+                                    ->inRandomOrder()
+                                    ->first()
+                                    ->id, 
+                    'loan_type_id' => rand(1, 2),  // Pour varier entre prêt automobile et prêt immobilier
+                    'loan_amount' => rand(5000, 20000),  // Montant du prêt entre 5000 et 200000
+                    'interest_rate' => rand(3, 7) / 100,  // Taux d'intérêt entre 3% et 7%
+                    'payment_frequency' => $paymentFrequency,
+                    'loan_date' => $loanDate,
+                    'due_date' => $dueDate,
+                    'status' => $statuses[array_rand($statuses)]// Statut aléatoire parmi 'pending', 'validated', 'rejected'
+                ]);
+            }
+            
+        }
+    }
+}

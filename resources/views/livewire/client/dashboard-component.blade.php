d9587fb28b5d1ba1eb1e371484e80eda209a51e7
lionel <lokononlionel97@gmail.com>
2023-12-26 10:49:54 +0100

    Version client

--- resources/views/livewire/client/dashboard-component.blade.php
+++ resources/views/livewire/client/dashboard-component.blade.php
@@ -92,9 +92,9 @@
 
         <!-- Reports -->
         <div class="col-12">
-          <div class="card">
-
-            <!--<div class="filter">
+                    <!-- Website Traffic -->
+                    <div class="card">
+            <div class="filter">
               <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
               <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                 <li class="dropdown-header text-start">
@@ -105,70 +105,73 @@
                 <li><a class="dropdown-item" href="#">This Month</a></li>
                 <li><a class="dropdown-item" href="#">This Year</a></li>
               </ul>
-            </div>-->
-
-            <div class="card-body">
-              <h5 class="card-title">Rapport <span></span></h5>
-
-              <!-- Line Chart -->
-              <div id="reportsChart"></div>
-
-              <script>
-                document.addEventListener("DOMContentLoaded", () => {
-                  new ApexCharts(document.querySelector("#reportsChart"), {
-                    series: [{
-                      name: 'Sales',
-                      data: [31, 40, 28, 51, 42, 82, 56],
-                    }, {
-                      name: 'Revenue',
-                      data: [11, 32, 45, 32, 34, 52, 41]
-                    }, {
-                      name: 'Customers',
-                      data: [15, 11, 32, 18, 9, 24, 11]
-                    }],
-                    chart: {
-                      height: 350,
-                      type: 'area',
-                      toolbar: {
-                        show: false
-                      },
-                    },
-                    markers: {
-                      size: 4
-                    },
-                    colors: ['#4154f1', '#2eca6a', '#ff771d'],
-                    fill: {
-                      type: "gradient",
-                      gradient: {
-                        shadeIntensity: 1,
-                        opacityFrom: 0.3,
-                        opacityTo: 0.4,
-                        stops: [0, 90, 100]
-                      }
-                    },
-                    dataLabels: {
-                      enabled: false
-                    },
-                    stroke: {
-                      curve: 'smooth',
-                      width: 2
-                    },
-                    xaxis: {
-                      type: 'datetime',
-                      categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
-                    },
-                    tooltip: {
-                      x: {
-                        format: 'dd/MM/yy HH:mm'
-                      },
-                    }
-                  }).render();
-                });
-              </script>
-              <!-- End Line Chart -->
             </div>
 
-          </div>
+            <div class="card-body pb-0">
+              <h5 class="card-title">Website Traffic <span>| Today</span></h5>
+
+              <div id="trafficChart" style="min-height: 400px;" class="echart"></div>
+
+              <script>
+    document.addEventListener("DOMContentLoaded", () => {
+        echarts.init(document.querySelector("#trafficChart")).setOption({
+            tooltip: {
+                trigger: 'item'
+            },
+            legend: {
+                top: '5%',
+                left: 'center'
+            },
+            series: [{
+                name: 'Résumé',
+                type: 'pie',
+                radius: ['40%', '70%'],
+                avoidLabelOverlap: false,
+                label: {
+                    show: false,
+                    position: 'center'
+                },
+                emphasis: {
+                    label: {
+                        show: true,
+                        fontSize: '18',
+                        fontWeight: 'bold'
+                    }
+                },
+                labelLine: {
+                    show: false
+                },
+                data: [
+                    {
+                        value: {{ $user->loan->where('status', 'rejected')->count() }},
+                        name: 'Prêt rejeté',
+                        itemStyle: {
+                            color: 'red' // Couleur rouge pour 'Prêt rejeté'
+                        }
+                    },
+                    {  
+                        value: {{ $user->operation->where('status', 'completed')->count() }},
+                        name: 'Transaction Terminée',
+                        itemStyle: {
+                            color: '#A3E571' // Couleur rouge pour 'Prêt rejeté'
+                        }
+                    },
+                    { 
+                        value: {{ $user->operation->where('status', 'pending')->count() }},
+                        name: 'Transaction encour',
+                        itemStyle: {
+                            color: '#FFDB58' // Couleur rouge pour 'Prêt rejeté'
+                        }
+                    }
+                ]
+            }]
+        });
+    });
+</script>
+
+
+            </div>
+          </div><!-- End Website Traffic -->
         </div><!-- End Reports -->
       </div>
     </div><!-- End Left side columns -->
